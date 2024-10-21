e5275bffec12a980e19eac94d8c76449e44a6c94
saurabhmaurya112 <saurabh.maurya@samespace.com>
2024-10-22 00:48:59 +0530

    feat: added Tabs

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,33 @@
+import { cn } from "@utils/common";
+import React from "react";
+
+const Tabs = ({ tabList, handleNavigation, activeTab, badgeClass }) => {
+  return (
+    <>
+      {tabList?.map((item, index) => (
+        <button
+          key={item.label + index}
+          className={`relative px-4 py-2 rounded-sm font-medium transition-colors duration-200 text-nowrap  ${
+            activeTab === item.label
+              ? " text-blue-900 hover:bg-blue-100 border-b-2 border-blue-900"
+              : " text-gray-500 hover:text-gray-900 hover:bg-gray-100"
+          }`}
+          onClick={() => handleNavigation(item)}
+        >
+          {item.label}
+          {item.badge && (
+            <div
+              className={cn("p-2 rounded-full absolute top-0 right-0", {
+                [badgeClass]: !!badgeClass,
+              })}
+            >
+              {item.badge}
+            </div>
+          )}
+        </button>
+      ))}
+    </>
+  );
+};
+
+export default Tabs;
--- src/components/UserNav.jsx
+++ src/components/UserNav.jsx
@@ -1,53 +1,52 @@
-import React, { useState } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import Tabs from "./Tabs";
 
 const detailsTabs = [
-    {
-        label: "Basic Details",
-        url: "/basic-details"
-    },
-    {
-        label: "Account Details",
-        url: "/account-details"
-    },
-    {
-        label: "Lead Details",
-        url: "/lead-details"
-    },
-    {
-        label: "Team",
-        url: "/team"
-    },
-    {
-        label: "Other Contacts",
-        url: "/other-contacts"
-    }
-]
+  {
+    label: "Basic Details",
+    url: "/basic-details",
+  },
+  {
+    label: "Account Details",
+    url: "/account-details",
+  },
+  {
+    label: "Lead Details",
+    url: "/lead-details",
+  },
+  {
+    label: "Team",
+    url: "/team",
+  },
+  {
+    label: "Other Contacts",
+    url: "/other-contacts",
+  },
+];
 const UserNav = () => {
-    const navigate = useNavigate()
-    const [searchParams] = useSearchParams();
-    const currentSection = searchParams.get('section');
-    const [activeDetailsTab, setActiveDetailsTab] = useState(detailsTabs.find(item=>item.url === `/${currentSection}`)?.label ?? "Basic Details"); // Default active details 
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const currentSection = searchParams.get("section");
+  const [activeDetailsTab, setActiveDetailsTab] = useState(
+    detailsTabs.find((item) => item.url === `/${currentSection}`)?.label ??
+      "Basic Details"
+  ); // Default active details
 
-    const handleNavigation = (item) => {
-        setActiveDetailsTab(item.label)
-        navigate(`?section=${item.url.slice(1)}`);
-    }
+  const handleNavigation = (item) => {
+    setActiveDetailsTab(item.label);
+    navigate(`?section=${item.url.slice(1)}`);
+  };
 
-    return <div className=' flex w-full px-2 overflow-x-auto no-scrollbar mr-4 lg:mr-0'>
-        {detailsTabs.map((item, index) => (
-            <button
-                key={index}
-                className={`px-4 py-2 rounded-sm font-medium transition-colors duration-200 text-nowrap  ${activeDetailsTab === item.label
-                    ? " text-blue-900 hover:bg-blue-100 border-b-2 border-blue-900"
-                    : " text-gray-500 hover:text-gray-900 hover:bg-gray-100"
-                    }`}
-                onClick={() => handleNavigation(item)}
-            >
-                {item.label}
-            </button>
-        ))}
+  return (
+    <div className="flex w-full px-2 overflow-x-auto no-scrollbar mr-4 lg:mr-0">
+      <Tabs
+        tabList={detailsTabs}
+        handleNavigation={handleNavigation}
+        activeTab={activeDetailsTab}
+      />
     </div>
-}
+  );
+};
 
-export default UserNav
+export default UserNav;
